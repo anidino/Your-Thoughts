7ecc0ebecd3cf3cf39964213ecc64be1583d00ab
AniElaina <ani.elaina.carr@gmail.com>
2021-09-17 13:45:11 -0400

    User model set up

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: 'Username is required',
+        trimmed: true 
+    },
+    email: {
+        type: String,
+        required: 'email is required',
+        unique: true,
+        validate: [validateEmail, 'Please use a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please use a valid email address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'thoughts',
+        },
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+    ],
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false,  
+},
+);
+
+// get friend count of user 
+UserSchema.virtual('friendCount').get(function () {
+    return this.comments.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
